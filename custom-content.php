fed05f2e47f805c4ee1a5f542553f958635599cc
BeardedGinger <josh@limecuda.com>
2015-01-05 00:22:26 -0500

    update plugin text header

---
+++ custom-content.php
@@ -0,0 +1,10 @@
+<?php
+/**
+* Plugin Name: Custom Content
+* Plugin URI: http://joshmallard.com
+* Description: Simple way to add custom content for client projects
+* Author: Josh Mallard
+* Version: 1.0.0
+* Author URI: http://joshmallard.com
+*/
+
