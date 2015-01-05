83a3ce33ea804d995a79d5d09cc82b991ca4c8ff
BeardedGinger <josh@limecuda.com>
2015-01-05 00:28:49 -0500

    load helper classes

--- custom-content.php
+++ custom-content.php
@@ -8,3 +8,13 @@
 * Author URI: http://joshmallard.com
 */
 
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+
+// Load CPT Helper
+require_once( plugin_dir_path( __FILE__ ) . 'core-classes/CPT_Core/CPT_Core.php' );
+
+// Load Taxonomy Helper
+require_once( plugin_dir_path( __FILE__ ) . 'core-classes/Taxonomy_Core/Taxonomy_Core.php' );
